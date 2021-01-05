96a63ebff111de9044f714dca38772bdec31ec29
GYUTAEKIM <kkt219a@gmail.com>
2021-01-06 02:54:23 +0900

    Persistence Context

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -32,11 +32,66 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            em.remove(findMember);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            for(Member member: result){
-                System.out.println("member.getName() = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//            for(Member member: result){
+//                System.out.println("member.getName() = " + member.getName());
+//            }
+
+
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속
+//            System.out.println("==before==");
+//            em.persist(member);
+//            //em.detach(member);// 영속성 컨텍스트 삭제
+//            System.out.println("==After==");
+//
+//            //1차캐시 조회
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+
+
+//            //영속, 1차 확인
+//            Member member1 = em.find(Member.class, 101L);
+//            Member member2 = em.find(Member.class, 101L);
+//            System.out.println("result: "+ (member1==member2));
+
+
+//            //영속
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("===============");
+
+
+//            //Update 테스트
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("zzzzz");
+
+
+//            //즉시 입력
+//            Member member = new Member(200L,"member200");
+//            em.persist(member);
+//            em.flush();
+//            System.out.println("===============");
+
+
+//            //준영속 만들기
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("AAAAA");
+//            //em.detach(member);
+//            //em.clear();
+//            Member member2 = em.find(Member.class, 150L);
+//            System.out.println("===============");
+
             tx.commit();
         }catch(Exception e){
             tx.rollback();
