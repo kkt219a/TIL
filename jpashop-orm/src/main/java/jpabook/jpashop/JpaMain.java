14a072223255f71da73431e988474ef0c7ad2851
GYUTAEKIM <kkt219a@gmail.com>
2021-01-09 00:28:58 +0900

    practical exmaple4 Inheritance mapping

--- jpashop-orm/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop-orm/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -22,10 +23,10 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
-
-            OrderItem orderItem = new OrderItem();
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
 
             tx.commit();
         }catch(Exception e){
