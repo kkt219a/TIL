f27058ae1b4d4b0dc10ef223a177746eb4669b83
GYUTAEKIM <kkt219a@gmail.com>
2021-01-10 02:43:21 +0900

    (jpql)basic grammer + query API

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result.getUsername() = " + result.getUsername());
+            
+
+            tx.commit();
+
+        }catch(Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+}
