8758772cfef8a3260a8350086da854beacb40ef8
GYUTAEKIM <kkt219a@gmail.com>
2021-01-10 03:08:05 +0900

    projection

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -22,10 +22,16 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result.getUsername() = " + result.getUsername());
+            em.flush();
+            em.clear();
+
+            //영속성 컨텍스트에 관리가 된다
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
             
 
             tx.commit();
