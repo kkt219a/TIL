306da6922777bc83ebce54243f8f502bd42e6be2
GYUTAEKIM <kkt219a@gmail.com>
2021-01-11 02:10:05 +0900

    path expression

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -19,37 +20,29 @@ public class JpaMain {
         try {
 
             Team team = new Team();
-            team.setName("teamA");
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setMemberType(MemberType.USER);
-
+            member.setUsername("관리자1");
             member.setTeam(team);
-
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setTeam(team);
+            em.persist(member2);
+
 
             em.flush();
             em.clear();
 
 
-            String query0 = "select concat('a','b') From Member m"; //ab 출력
-            String query1 = "select substring(m.username,2,3) From Member m";
-            String query2 = "select locate('de','abcdef') From Member m"; // 4출력
-            String query3 = "select size(t.members) From Team t"; // 1출력
-
-            String query = "select function('group_concat',m.username) From Member m"; // 1출력
-
-
+            String query = "select m.username from Team t join t.members m";
 
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
 
