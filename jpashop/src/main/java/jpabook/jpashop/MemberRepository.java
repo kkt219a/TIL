9bcb55af51ef7fb039946991a7c654b7e76888dc
GYUTAEKIM <kkt219a@gmail.com>
2021-01-12 01:37:06 +0900

    project Preferences

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // 스프링 부트가 엔티티매니져 자동으로 만들어줌
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
+}
