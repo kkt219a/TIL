bfe1499506129951d990a785d9df0907e7ade433
GYUTAEKIM <kkt219a@gmail.com>
2021-01-14 01:35:39 +0900

    success order

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -53,7 +54,7 @@ public class OrderService {
      * 주문 취소
      */
     @Transactional
-    public void cancerOrder(Long orderId){
+    public void cancelOrder(Long orderId){
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
         //주문 취소
@@ -61,7 +62,8 @@ public class OrderService {
     }
 
     //검색
-//    public List<Order> findOrders(OrderSearch orderSearch){
-//
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
+
 }
