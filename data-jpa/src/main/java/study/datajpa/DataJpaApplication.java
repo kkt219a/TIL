02193cb53ba471ccdd1a732e1dcaed9e68cf2a14
GYUTAEKIM <kkt219a@gmail.com>
2021-01-18 14:30:45 +0900

    Extension Function last

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +17,11 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		//interface에서 메소드가 하나면 오버라이드 생략가능 람다로!
+		// 스프링 시큐리티 컨텍스트, 세션정보 가져와서 걔 아이디 꺼내야함
+		// 아니면 http세션에서 꺼내서 확인하거나 그렇게
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberTest {
 
     @PersistenceContext EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() throws Exception {
@@ -47,7 +50,27 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.getTeam() = " + member.getTeam());
         }
+    }
 
+    @Test
+    public void JpaEventBaseEntity() throws Exception{
+        //given
+        Member member = new Member("member");
+        memberRepository.save(member);
 
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.getCreatedDate() = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdatedDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy() = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
     }
 }
