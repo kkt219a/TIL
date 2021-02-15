b715abfceed17756992975c8d823dd62c4a5a7c7
GYUTAEKIM <kkt219a@gmail.com>
2021-02-16 01:45:59 +0900

    Restful service Init

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.example.restfulwebservice.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //GET
+    // /hello-world (endpoint)
+    //
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    // alt + enter
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+//    @GetMapping(path = "/hello-world-internationalized")
+//    public String helloWorldInternationalized(
+//            @RequestHeader(name="Accept-Language", required=false) Locale locale) {
+//        return messageSource.getMessage("greeting.message", null, locale);
+//    }
+
+}
