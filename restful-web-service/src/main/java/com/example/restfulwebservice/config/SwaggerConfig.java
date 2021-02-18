e37498d04bf53fb0429715d82f362c2e3a4e8c69
GYUTAEKIM <kkt219a@gmail.com>
2021-02-18 23:45:23 +0900

    contact info

--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,10 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private static final Contact DEFAULT_CONTACT  = new Contact("Kenneth Lee",
+            "http://www.joneconsulting.co.kr", "edowon@joneconsulting.co.kr");
+    
 
     @Bean
     public Docket api(){
