2b6aa47b328c454ebc041bb2fd7f3f250ef28825
GYUTAEKIM <kkt219a@gmail.com>
2021-02-19 03:23:52 +0900

    spring data jpa utilize

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/Post.java
@@ -0,0 +1,26 @@
+package com.example.restfulwebservice.user;
+
+import com.example.restfulwebservice.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+}
